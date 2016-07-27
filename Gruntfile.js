61e02a19af141aff4a43048b2c36757207903f7a
Andrey Lavrik <lavrik.aw@ya.ru>
2016-07-27 17:19:27 +0300

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 
 module.exports = function(grunt) {
 
-    // 1. Вся настройка находится здесь
     grunt.initConfig ({
         pkg: grunt.file.readJSON('package.json'),
 
@@ -53,7 +52,7 @@ module.exports = function(grunt) {
                   optimization: 2
                 },
                 files: {
-                  "css/style.css": "less/style.less" // destination file and source file
+                  "css/style.css": "less/style.less" 
                 }
               }
             }
@@ -62,14 +61,13 @@ module.exports = function(grunt) {
 
     });
 
-    // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
 
-    // 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
+
     grunt.registerTask('default', ['concat','uglify','imagemin','less','watch']);
 
 };
